c0030776f61f6567deab65c2132eb01235116711
Oskar Henriksson <oskar.henriksson2001@gmail.com>
2020-09-07 14:15:16 +0200

    Naive board implementation

--- rustchessian/src/lib.rs
+++ rustchessian/src/lib.rs
@@ -5,3 +5,11 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+pub mod board {
+    pub fn init_board(){
+        let mut board_matrix =  [[0; 8]; 8];
+        println!("Board: {:?}", board_matrix);
+    }
+}
+
