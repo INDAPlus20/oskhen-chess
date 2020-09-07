b3d89f7d20d2d624f81c1f2058694941f0ac3f5d
Oskar Henriksson <oskar.henriksson2001@gmail.com>
2020-09-07 15:04:13 +0200

    Board and Unit definitions added

--- rustchessian/src/lib.rs
+++ rustchessian/src/lib.rs
@@ -6,8 +6,31 @@ mod tests {
     }
 }
 
+pub mod Units {
+    pub enum Pieces{
+        Empty,
+        Pawn,
+        Rook,
+        Knight,
+        Bishop,
+        Queen,
+        King,
+    }
+}
+
 pub mod board {
+use super::Units;
+    struct square {
+        Piece: Units::Pieces,
+        Team: i8,
+    }
+
+    struct Board {
+        matrix: [[square; 8]; 8],
+    }
+
     pub fn init_board(){
+    
         let mut board_matrix =  [[0; 8]; 8];
         println!("Board: {:?}", board_matrix);
     }
